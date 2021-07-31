3766e15d98aead4146f0eb972fb7490495f08283
Iaro <iaroslav.slovak@gmail.com>
2021-07-31 23:21:28 +0300

    Initial commit

--- src/main/java/fasttrackit/ro/library/entity/Readers.java
+++ src/main/java/fasttrackit/ro/library/entity/Readers.java
@@ -5,11 +5,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity()
 public class Readers {
     @Id
     @Column(name = "reader_id")
-    @GeneratedValue()
+    @GeneratedValue(strategy = IDENTITY)
     private Long readerId;
     @Column(name = "card_id")
     private String libraryCard;
@@ -22,5 +24,74 @@ public class Readers {
     private String phone;
 
     public Readers() {
+    }
+
+    public Long getReaderId() {
+        return readerId;
+    }
+
+    public void setReaderId(Long readerId) {
+        this.readerId = readerId;
+    }
+
+    public String getLibraryCard() {
+        return libraryCard;
+    }
+
+    public void setLibraryCard(String libraryCard) {
+        this.libraryCard = libraryCard;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Readers{" +
+                "readerId=" + readerId +
+                ", libraryCard='" + libraryCard + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
     }
 }
